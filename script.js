f4b155aed815b871c81c1e40b856c3d7425125c1
Aniket Bamotra <aniketbamotra.11@gmail.com>
2024-08-22 21:32:02 +0530

    responsive pages: home, our platform, about

--- script.js
+++ script.js
@@ -14,3 +14,57 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+
+// Mobile menu
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var overlay = document.getElementById("overlay");
+
+    if (menu.style.width === "100%") {
+        menu.style.width = "0";
+        overlay.style.opacity = "0";
+        setTimeout(function() {
+            overlay.style.display = "none";
+        }, 500);
+    } else {
+        menu.style.width = "100%";
+        overlay.style.display = "block";
+        setTimeout(function() {
+            overlay.style.opacity = "1";
+        }, 10);
+    }
+}
+
+
+// Carousal 
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Auto slide
+setInterval(nextSlide, 3000);
